68279f17bdb2aecc0c647f814c8dff9c10e4ee22
Seok Chan Chung <csckhw@gmail.com>
2021-06-13 12:03:43 -0400

    connected with react

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import mysql.connector
 from flask import jsonify
 
 app = flask.Flask(__name__)
+
 app.config["DEBUG"] = True
 
 
@@ -11,22 +12,35 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
+class create_dict(dict): 
+  
+    # __init__ function 
+    def __init__(self): 
+        self = dict() 
+          
+    # Function to add key:value 
+    def add(self, key, value): 
+        self[key] = value
 
-@app.route('/get2ginfos', methods=['GET'])
+@app.route('/announcedinfos', methods=['GET'])
 def get2ginfos():
   cnx = mysql.connector.connect(user='root', password='Skydrive0404', host='127.0.0.1', database='db_phones',auth_plugin='mysql_native_password')
   cursor = cnx.cursor()
 
-  query = ("SELECT * FROM db_phones.2gbandstable")
-
+  query = ("SELECT announcedInfo, count(uniqueID) as count  FROM db_phones.announcedtable group by (announcedInfo)")
   cursor.execute(query)
-
   rows = cursor.fetchall()
-  resp=jsonify(rows)
-  
-  return resp
 
+  results=[]
+  for row in rows:
+    results.append({"y":row[1],"name":row[0]})
+
+  resp=jsonify(results)
+  resp.headers.add('Access-Control-Allow-Origin', '*')
   cursor.close()
   cnx.close()
+  return resp
+
+  
 
 app.run()
