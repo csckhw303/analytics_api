7988226f552e242c0526848a21120f7034c05cff
Seok Chan Chung <csckhw@gmail.com>
2021-06-17 15:47:38 -0400

    update

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
+from os import abort
 import flask
 import datetime
 import mysql.connector
 from flask import jsonify
-
+from flask import request, make_response
 app = flask.Flask(__name__)
 
 app.config["DEBUG"] = True
@@ -42,7 +43,7 @@ def getannouncedinfos():
   return resp
 
 @app.route('/2Gbandsinfos', methods=['GET'])
-def get2ginfos():
+def get2GBandinfos():
   cnx = mysql.connector.connect(user='root', password='Skydrive0404', host='127.0.0.1', database='db_phones',auth_plugin='mysql_native_password')
   cursor = cnx.cursor()
 
@@ -60,6 +61,82 @@ def get2ginfos():
   cnx.close()
   return resp
 
-  
+@app.route('/SIMinfos', methods=['GET'])
+def getSIMinfos():
+  cnx = mysql.connector.connect(user='root', password='Skydrive0404', host='127.0.0.1', database='db_phones',auth_plugin='mysql_native_password')
+  cursor = cnx.cursor()
+
+  query = ("SELECT SIM, count(uniqueID) as count  FROM db_phones.SIMTable group by (SIM)")
+  cursor.execute(query)
+  rows = cursor.fetchall()
+
+  results=[]
+
+  for row in rows:
+    results.append({"y":row[1],"name":row[0]})
+
+  resp=jsonify(results)
+  resp.headers.add('Access-Control-Allow-Origin', '*')
+  cursor.close()
+  cnx.close()
+  return resp
+
+@app.route('/wlaninfos', methods=['GET'])
+def getwlaninfos():
+  cnx = mysql.connector.connect(user='root', password='Skydrive0404', host='127.0.0.1', database='db_phones',auth_plugin='mysql_native_password')
+  cursor = cnx.cursor()
+
+  query = ("SELECT wlan, count(uniqueID) as count  FROM db_phones.wlanTable group by (wlan)")
+  cursor.execute(query)
+  rows = cursor.fetchall()
+
+  results=[]
+
+  for row in rows:
+    results.append({"y":row[1],"name":row[0]})
+
+  resp=jsonify(results)
+  resp.headers.add('Access-Control-Allow-Origin', '*')
+  cursor.close()
+  cnx.close()
+  return resp
+
+@app.route('/search', methods=['OPTIONS','POST'])
+def search():
+
+
+  if request.method == 'OPTIONS': 
+        return build_preflight_response()
+  elif request.method == 'POST': 
+        req = request.get_json()
+        app.logger.info(req)
+        cnx = mysql.connector.connect(user='root', password='Skydrive0404', host='127.0.0.1', database='db_phones',auth_plugin='mysql_native_password')
+        cursor = cnx.cursor()
+
+        query = ("SELECT *  FROM db_phones.phones where brand LIKE '%" + req['title'] + "%'")
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        results=[]
+
+        for row in rows:
+           results.append({"y":row[1],"name":row[0]})
+
+        resp=jsonify(results)
+        cursor.close()
+        cnx.close()
+        return build_actual_response(resp)
+
+def build_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+def build_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
 
 app.run()
